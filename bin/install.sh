25d4a164a20ee76b29e50133402980927d3c97ea
ia17011 <cfoyvlird@gmail.com>
2020-02-05 11:00:44 +0900

    update

--- bin/install.sh
+++ bin/install.sh
@@ -10,29 +10,14 @@ set -u
 DOTPATH=~/.dotfiles
 GITHUB_URL=https://github.com/ia17011/dotfiles
 
-# git が使えるなら git
-if has "git"; then
-    git clone --recursive "$GITHUB_URL" "$DOTPATH"
+# add submodule
+git submodule update --init --recursive
 
-# 使えない場合は curl か wget を使用する
-elif has "curl" || has "wget"; then
-    tarball="https://github.com/ia17011/dotfiles/archive/master.tar.gz"
-
-    # どっちかでダウンロードして，tar に流す
-    if has "curl"; then
-        curl -L "$tarball"
-
-    elif has "wget"; then
-        wget -O - "$tarball"
-
-    fi | tar zxv
-
-    mv -f dotfiles-master "$DOTPATH"
-
-else
-    die "curl or wget required"
-fi
-
+# prezto
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
 
 cd ~/.dotfiles
 if [ $? -ne 0 ]; then
@@ -44,14 +29,13 @@ for f in .??*;do
     [ "$f" = ".git" ] && continue
 	[ "$f" = ".config"] && continue
 	[ "$f" = ".env" ] && continue
-    [ "$f" = ".require_oh-my-zsh" ] && continue
     [ "$f" = ".gitmodules" ] && continue
 
-    ln -snfv "$DOTPATH/$f" "$HOME"/"$f"
+    ln -snfv "$DOTPATH/$f" ~/"$f"
 done
 
+chsh -s $(which zsh)
 
-
-curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
-chsh -s /bin/zsh
+source "$DOTPATH"/.zshrc
+source "$DOTPATH"/.zpreztorc
 
